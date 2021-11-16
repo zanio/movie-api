54699a9a69c3bc670e97a74d22488fd7437b5ede
Aniefiok Akpan <akp.ani@yahoo.com>
2021-11-16 08:14:37 +0100

    chore(db): update the db configuration

--- src/persistence/db.js
+++ src/persistence/db.js
@@ -5,5 +5,4 @@ dotenv.config();
 module.exports = new Pool({
   max: 10,
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.ssl||false
-});
+  ssl: process.env.NODE_ENV!=='development' ? { rejectUnauthorized: false } : false});
